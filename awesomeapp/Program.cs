16ce84ae923ca6ea86af8d82114ace3e9c85749a
Fanie Reynders <fanie@reynders.co>
2015-12-08 10:42:08 +0100

    add awesomeapp

---
+++ awesomeapp/Program.cs
@@ -0,0 +1,13 @@
+﻿using System;
+
+namespace AwesomeApp
+{
+    public class Program
+    {
+        public static void Main(string[] args)
+        {
+            var logo = Logo.Draw(Environment.GetEnvironmentVariable("OS"));
+            Console.WriteLine(logo);
+        }
+    }
+}
